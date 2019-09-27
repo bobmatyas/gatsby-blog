f1744a5c7a49d0a310cb0ac9f4d79812e4259b90
bobmatyas <ballium@protonmail.com>
2019-09-26 20:59:40 -0400

    mobile adjustments

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 
@@ -7,32 +7,44 @@ import { Link } from "gatsby"
 export default () => {
 
 
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(open);
 
-  const updateDimensions = ()=> {
-    if(window.innerWidth > 700) {
-      console.log(`big window`);
-      const removeMenu = document.getElementsByClassName('main__menu__overlay--open');
-      console.log(removeMenu);
-      // removeMenu.classList.remove("main__menu__overlay--open");
-      // removeMenu.classList.add("main__menu__overlay");
-    }
-  }
-  window.addEventListener("resize", updateDimensions());
+  const [width, setWidth] = useState(window.innerWidth);  
+  
+  let [styles, setStyles] = useState('main__menu__overlay');
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth)
+      console.log(width);
+      if (width > 700) {
+        setStyles = 'main__menu__overlay';
+      } else if (width < 699) {
+        setStyles = 'main__menu__overlay--open';
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    console.log(width);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
 
   return (
     <div className="main__menu__right">
     <nav className="main__menu__holder">
       <div className="main__menu__toggle" onClick={() => setOpen(!open)}>
         <div className="main__menu__icon">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>    
+          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>    
           <span className="main__menu__text">menu</span>
         </div>
         
       </div>
 
-      <div className={open ? "main__menu__overlay--open" : "main__menu__overlay"}>
-        <ul className={open ? "main__menu" : "main__menu main__menu--hidden "}>
+      <div className={open ? "main__menu__overlay--open" : "main__menu__overlay" }>
+        <ul className={open ? "main__menu" : "main__menu main__menu--hidden"}>
           <li>
             <Link
               to={`/about/`}
