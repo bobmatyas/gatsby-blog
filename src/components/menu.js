32bec372243105edb0703e2dc33519540415613d
bobmatyas <ballium@protonmail.com>
2019-10-24 17:28:16 -0400

    add button for menu toggle

--- src/components/menu.js
+++ src/components/menu.js
@@ -10,13 +10,13 @@ export default () => {
   return (
     <div className="main__menu__right">
     <nav className="main__menu__holder">
-      <a className="main__menu__toggle" onClick={() => setOpen(!open)}>
+      <button className="main__menu__toggle" onClick={() => setOpen(!open)}>
         <div className="main__menu__icon">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>    
-          <span className="main__menu__text">menu</span>
+          <svg width="15" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>    
+          <div className="main__menu__text">menu</div>
         </div>
         
-      </a>
+      </button>
 
       <div className={open ? "main__menu__overlay--open" : "main__menu__overlay" }>
         <ul className={open ? "main__menu" : "main__menu main__menu--hidden"}>
