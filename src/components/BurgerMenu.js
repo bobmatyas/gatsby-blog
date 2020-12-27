3c236931f13d5b5775999c6ced8118886d4494e7
bob <45246438+bobmatyas@users.noreply.github.com>
2020-12-27 16:03:38 -0500

    add react-burger

---
+++ src/components/BurgerMenu.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Menu from 'react-burger-menu/lib/menus/slide'
+import { Link } from 'gatsby'
+import './BurgerMenu.css'
+
+const BurgerMenu = () => {
+    return (
+      <Menu noOverlay isOpen={ false } right>
+        <ul style={{color: 'white'}}> 
+          <li>
+            <Link
+              to={`/about/`}
+            >
+              About
+        </Link>
+          </li>
+          <li>
+            <Link
+              to={`/portfolio/`}
+            >
+              Portfolio
+        </Link>
+          </li>
+          <li>
+            <Link
+              to={`/blog/`}
+            >
+              Blog
+        </Link>
+          </li>
+          <li>
+            <Link
+              to={`/contact/`}
+            >
+              Contact
+        </Link>
+          </li>
+        </ul>
+      </Menu>
+    )
+  }
+  
+  export default BurgerMenu
