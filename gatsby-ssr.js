2448765c3efb4b7ce8e035c37c9b7331cb160ec0
bob <45246438+bobmatyas@users.noreply.github.com>
2021-01-02 13:12:40 -0500

    add cloudflare analytics

---
+++ gatsby-ssr.js
@@ -0,0 +1,9 @@
+import React from "react";
+export function onRenderBody( { setPostBodyComponents } ) {
+    setPostBodyComponents([
+        <script 
+        defer 
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "9c46957e3f89451a8c879dc672753cfa"}'></script>,
+    ]);
+}
