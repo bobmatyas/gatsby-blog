ee828ece2481b7ed5a002eaf292397e92fa09f24
bob <45246438+bobmatyas@users.noreply.github.com>
2021-08-20 20:57:35 -0400

    fix analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,17 @@
 import React from "react";
 export function onRenderBody( { setPostBodyComponents } ) {
-    setPostBodyComponents([
-        <script 
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon="{'token': '9c46957e3f89451a8c879dc672753cfa'}"
-        defer 
-        ></script>,
-    ]);
+    setPostBodyComponents(
+    [React.createElement(`div`, {
+        key: `cloudflare-analytics`,
+        dangerouslySetInnerHTML: {
+          __html: `
+            <!-- Cloudflare Web Analytics -->
+            <script defer src='https://static.cloudflareinsights.com/beacon.min.js'
+              data-cf-beacon='{"token": "9c46957e3f89451a8c879dc672753cfa"}'>
+            </script>
+            <!-- End Cloudflare Web Analytics -->
+        `
+        }
+      })
+    ])
 }
