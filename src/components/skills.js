a2d7cdd9f86148871a4866fb4355be22258e8519
bob <45246438+bobmatyas@users.noreply.github.com>
2021-12-06 20:46:38 -0500

    update skills component

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,4 +1,29 @@
 import React from "react"
+import styled from "styled-components"
+
+
+const SkillsHeader = styled.h4`
+  border-bottom: 1px solid #eee;
+`;
+
+const SkillsList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  margin: 0 0 25px 0;
+  max-width: 800px;
+  padding: 0;
+`;
+
+const SkillsListItem = styled.li`
+  color: #1d1b1b;
+  background-color: #F3C5FF;
+  border-radius: 5px;
+  font-size: 0.8rem;
+  margin: 5px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+`;
 
 
 const Skills = () => (
@@ -6,47 +31,47 @@ const Skills = () => (
  
     <h3 className="skills__header">Skills</h3>
 
-    <h4 className="skills__header__sub">Web Development</h4>
+    <SkillsHeader>Web Development</SkillsHeader>
 
-    <ul className="skills__blob">
-      <li className="skills__blob__item">HTML</li>
-      <li className="skills__blob__item">CSS</li>
-      <li className="skills__blob__item">JavaScript / ES6</li>
-      <li className="skills__blob__item">jQuery</li>
-      <li className="skills__blob__item">AngularJS</li>
-      <li className="skills__blob__item">ReactJS</li>
-      <li className="skills__blob__item">SASS</li>
-      <li className="skills__blob__item">Git</li>
-      <li className="skills__blob__item">MySQL</li>
-      <li className="skills__blob__item">PostgreSQL</li>
-      <li className="skills__blob__item">PHP</li>
-      <li className="skills__blob__item">NodeJS</li>
-      <li className="skills__blob__item">Gulp</li>
-      <li className="skills__blob__item">Adobe CC</li>
-    </ul>
+    <SkillsList>
+      <SkillsListItem>HTML</SkillsListItem>
+      <SkillsListItem>CSS</SkillsListItem>
+      <SkillsListItem>JavaScript / ES6</SkillsListItem>
+      <SkillsListItem>jQuery</SkillsListItem>
+      <SkillsListItem>AngularJS</SkillsListItem>
+      <SkillsListItem>ReactJS</SkillsListItem>
+      <SkillsListItem>SASS</SkillsListItem>
+      <SkillsListItem>Git</SkillsListItem>
+      <SkillsListItem>MySQL</SkillsListItem>
+      <SkillsListItem>PostgreSQL</SkillsListItem>
+      <SkillsListItem>PHP</SkillsListItem>
+      <SkillsListItem>NodeJS</SkillsListItem>
+      <SkillsListItem>Gulp</SkillsListItem>
+      <SkillsListItem>Adobe CC</SkillsListItem>
+    </SkillsList>
   
-    <h4 className="skills__header__sub">WordPress</h4>
+    <SkillsHeader>WordPress</SkillsHeader>
 
-    <ul className="skills__blob">
-      <li className="skills__blob__item">Theme design and development</li> 
-      <li className="skills__blob__item">Custom themes</li>
-      <li className="skills__blob__item">Security</li>
-      <li className="skills__blob__item">Maintenance</li>
-      <li className="skills__blob__item">Deployment</li>
-      <li className="skills__blob__item">Optimization</li>
-    </ul>
+    <SkillsList>
+      <SkillsListItem>Theme design and development</SkillsListItem> 
+      <SkillsListItem>Custom themes</SkillsListItem>
+      <SkillsListItem>Security</SkillsListItem>
+      <SkillsListItem>Maintenance</SkillsListItem>
+      <SkillsListItem>Deployment</SkillsListItem>
+      <SkillsListItem>Optimization</SkillsListItem>
+    </SkillsList>
 
-    <h4 className="skills__header__sub">Content Creation</h4>
+    <SkillsHeader>Content Creation</SkillsHeader>
       
-    <ul className="skills__blob">
-      <li className="skills__blob__item">Copy writing</li> 
-      <li className="skills__blob__item">Copy editing</li>
-      <li className="skills__blob__item">Media strategy</li>
-      <li className="skills__blob__item">Press releases</li>
-      <li className="skills__blob__item">SEO (Search Engine Optimization)</li>
-      <li className="skills__blob__item">Social media</li>
-      <li className="skills__blob__item">Blogging</li>
-    </ul>
+    <SkillsList>
+      <SkillsListItem>Copy writing</SkillsListItem> 
+      <SkillsListItem>Copy editing</SkillsListItem>
+      <SkillsListItem>Media strategy</SkillsListItem>
+      <SkillsListItem>Press releases</SkillsListItem>
+      <SkillsListItem>SEO (Search Engine Optimization)</SkillsListItem>
+      <SkillsListItem>Social media</SkillsListItem>
+      <SkillsListItem>Blogging</SkillsListItem>
+    </SkillsList>
 
   </div>
 )
