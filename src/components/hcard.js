24b1cb92d1189e9a0a4d66b03c7c4e94c2ed75d6
bob <45246438+bobmatyas@users.noreply.github.com>
2021-12-06 21:33:46 -0500

    remove sass dependency

--- src/components/hcard.js
+++ src/components/hcard.js
@@ -8,12 +8,12 @@ const Hcard = () => (
   <section style={styleObj} className="h-card">
 
     <span className="p-name">Bob Matyas</span>   
-    <a className="u-url u-uid" href="https://www.bobmatyas.com"></a>
-    <a className="u-url" rel="me" href="https://github.com/bobmatyas"></a>
-    <a className="u-url" rel="me" href="https://codepen.io/bobmatyas"></a>
-    <a className="u-url" rel="me" href="https://twitter.com/bobmatyas"></a>
-    <a className="u-url" rel="me" href="https://mastodon.social/@bobmatyas"></a>
-    <a className="u-url" rel="me" href="https://www.linkedin.com/in/bob-matyas"></a>
+    <a className="u-url u-uid" href="https://www.bobmatyas.com">BobMatyas.com</a>
+    <a className="u-url" rel="me" href="https://github.com/bobmatyas">Bobmatyas on GitHub</a>
+    <a className="u-url" rel="me" href="https://codepen.io/bobmatyas">Bobmatyas on CodePen</a>
+    <a className="u-url" rel="me" href="https://twitter.com/bobmatyas">Bobmatyas on Twitter</a>
+    <a className="u-url" rel="me" href="https://mastodon.social/@bobmatyas">Bobmatyas on Mastodon</a>
+    <a className="u-url" rel="me" href="https://www.linkedin.com/in/bob-matyas">Bobmatyas on Linkedin</a>
   </section>
 )
 
