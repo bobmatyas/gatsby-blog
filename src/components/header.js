0e40b7d12be47c1e61217d5c2d3077024a3be276
bob <45246438+bobmatyas@users.noreply.github.com>
2021-12-06 21:28:47 -0500

    update logo component

--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,29 @@ import Menu from "../components/menu"
 import styled from "styled-components"
 
 const HeaderStyled = styled.header`
-    align-items: center;
     background: #030507;
     border-top: 4px solid #845EC2;
+
+`;
+
+const HeaderContainer = styled.div`
+    align-items: center;
     display: flex;
     justify-content: space-between;
+    max-width: 1200px;
+    margin: 0 auto;
     padding-top: 15px;
     padding-bottom: 15px;
+    width: 100%;
 `;
 
 const Header = () => (
-  <HeaderStyled>
-        <Logo /> 
-        <Menu />
-  </HeaderStyled>
+    <HeaderStyled>
+        <HeaderContainer>
+            <Logo /> 
+            <Menu />
+        </HeaderContainer>
+    </HeaderStyled>
 )
 
 export default Header;
