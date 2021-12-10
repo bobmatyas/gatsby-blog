fc34e4f8b77067957d4ff39a5293ea295e8a3fd5
bob <45246438+bobmatyas@users.noreply.github.com>
2021-12-10 16:14:09 -0500

    convert blog index to styled components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,50 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Gradient from "../components/gradient"
+import styled from "styled-components"
+
+const BlogPost = styled.article`
+  border: 1px solid #eee;
+  border-radius: 5px;
+  margin: 0 0 25px 0;
+  padding: 25px;
+  -webkit-box-shadow: 0px 5px 10px 0px rgba(180,180,180,0.35);
+  -moz-box-shadow: 0px 5px 10px 0px rgba(180,180,180,0.35);
+  box-shadow: 0px 5px 10px 0px rgba(180,180,180,0.35);
+
+  :hover {
+    -webkit-box-shadow: 0px 5px 10px 0px rgba(0,0,0, 0.5);
+    -moz-box-shadow: 0px 5px 10px 0px rgba(0,0,0,0.5);
+    box-shadow: 0px 5px 10px 0px rgba(0,0,0,0.5);
+    cursor: pointer;
+  }
+
+  :hover h3 {
+    text-decoration: underline;
+  }
+`
+
+const BlogPostDate = styled.p`  
+  color: #666;
+  font-family: var(--system-fonts);
+  border-bottom: 1px solid #ccc;
+  font-variant: small-caps;
+  font-weight: bold;
+  font-size: 75%;
+  margin: 0;
+  padding: 0;
+  text-transform: uppercase;
+`;
+
+const BlogPostHeader = styled.h3`  
+  margin: 12.5px 0 12.5px 0;
+  padding: 5px 0;
+`;
+
+const BlogPostDescription = styled.p`
+  color: #666;
+`;
+
 
 const Blog = ({ data }) => {
   return (
@@ -12,32 +55,30 @@ const Blog = ({ data }) => {
       title="Blog"
       description="Blog posts from Bob Matyas, a web developer in Grand Rapids, Michigan"
     />
-    
-    <Gradient color="purple" />
-    
+        
     <div className="main__content">
 
     <h2 className="main__content__header">Blog</h2>
 
         { data.allMarkdownRemark.edges.map(({ node }) => (
-          <article className="blog__card h-entry" key={node.id}>
+          <BlogPost className="h-entry" key={node.id}>
             <Link
               to={node.fields.slug} className="u-uid u-url">
 
-              <p className="blog__card__date dt-published">
+              <BlogPostDate className="dt-published">
                 {node.frontmatter.date}
-              </p>
+              </BlogPostDate>
               
-              <h3 className="blog__card__header p-name">
+              <BlogPostHeader className="p-name">
                 {node.frontmatter.title}{" "}
-              </h3>
+              </BlogPostHeader>
             
-              <p className="blog__card__text p-summary">
+              <BlogPostDescription className="p-summary">
                 {node.frontmatter.description}
-              </p>
+              </BlogPostDescription>
               
             </Link>
-          </article>
+          </BlogPost>
         ))}
       </div>
     </Layout>
