7a17cd2a6c9c2c8a75662d0cac1b637bff7f519e
bob <45246438+bobmatyas@users.noreply.github.com>
2021-12-10 12:12:34 -0500

    add padding to page

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ import styled from "styled-components"
 const HeaderStyled = styled.header`
     background: #030507;
     border-top: 4px solid #845EC2;
-
+    padding: 0 5%;
 `;
 
 const HeaderContainer = styled.div`
