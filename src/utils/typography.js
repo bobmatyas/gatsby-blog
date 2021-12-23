e900593b7d5e62042ea86fef45f97eb79bd19de2
bob <45246438+bobmatyas@users.noreply.github.com>
2021-12-22 21:19:44 -0500

    got eslint working

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,5 @@
 import Typography from "typography"
 import wordpress2016Theme from "typography-theme-wordpress-2016"
-const typography = new Typography(
-    wordpress2016Theme
-)
+const typography = new Typography(wordpress2016Theme)
 
 export default typography
