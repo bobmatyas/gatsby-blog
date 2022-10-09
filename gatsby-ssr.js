d2d4d52cde59b076b9bc4683449e5a633b92b4d6
bob <45246438+bobmatyas@users.noreply.github.com>
2022-10-09 17:29:13 -0400

    add partytown for analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,20 @@
-import React from "react";
-export function onRenderBody( { setPostBodyComponents } ) {
-    setPostBodyComponents(
-    [React.createElement(`div`, {
+import React from "react"
+import { Partytown } from "@builder.io/partytown/react"
+
+export function onRenderBody({ setPostBodyComponents }) {
+  ;(<Partytown key="partytown" debug={true} forward={["dataLayer.push"]} />),
+    setPostBodyComponents([
+      React.createElement(`div`, {
         key: `cloudflare-analytics`,
         dangerouslySetInnerHTML: {
           __html: `
             <!-- Cloudflare Web Analytics -->
-            <script defer src='https://static.cloudflareinsights.com/beacon.min.js'
+            <script type="text/partytown" src='https://static.cloudflareinsights.com/beacon.min.js'
               data-cf-beacon='{"token": "9c46957e3f89451a8c879dc672753cfa"}'>
             </script>
             <!-- End Cloudflare Web Analytics -->
-        `
-        }
-      })
+        `,
+        },
+      }),
     ])
 }
