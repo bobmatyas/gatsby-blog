3136f6e765511a815ab06c7676405c355091f9cd
bob <45246438+bobmatyas@users.noreply.github.com>
2022-10-10 16:36:26 -0400

    draft header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import Logo from "./logo"
 import Menu from "./menu"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Header = () => {
   const headerBg = {
@@ -11,8 +11,18 @@ const Header = () => {
 
   return (
     <header className="border-t-green-700 border-t-8" style={headerBg}>
-      <div>
-        <Logo />
+      <div className="container m-auto flex flex-col items-center justify-center">
+        <StaticImage
+          src="../images/flower-logo.jpg"
+          alt="Flower"
+          imgClassName="u-photo"
+          className=" hcard__photo m-auto header__logo mx-10 py-6 px-5 rounded-full bg-white mt-10 border-gray-100 border-4"
+          style={{ width: "100px" }}
+        />
+
+        <h2 className="text-4xl font-extrabold mt-2 italic lowercase">
+          Bob Matyas
+        </h2>
         <Menu />
       </div>
     </header>
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -3,77 +3,6 @@ import { Link } from "gatsby"
 import BurgerMenu from "./BurgerMenu"
 import styled from "styled-components"
 
-const DesktopHolder = styled.nav`
-  display: none;
-
-  @media screen and (min-width: 800px) {
-    display: block;
-    padding: 15px 0 0 0;
-  }
-`
-
-const DesktopMenu = styled.ul`
-  display: flex;
-  font-size: unset;
-  font-weight: 500;
-  justify-content: flex-end;
-  list-style-type: none;
-  margin: 0;
-  padding: 0 3% 0 0;
-  text-transform: uppercase;
-
-  a {
-    display: inline-block;
-    padding: 0 0 0.5em 0;
-    color: rgba(255, 255, 255, 0.8);
-    font-size: 0.9rem;
-    position: relative;
-    letter-spacing: 1px;
-    text-decoration: none;
-  }
-
-  a:before,
-  a:after {
-    position: absolute;
-    -webkit-transition: all 0.07s ease;
-    transition: all 0.07s ease;
-  }
-
-  a:before {
-    bottom: 0;
-    display: block;
-    height: 3px;
-    width: 0%;
-    content: "";
-    background-color: var(--pink-color);
-  }
-  a:after {
-    left: 0;
-    top: 0;
-    padding: 0 0 0.5em 0;
-    position: absolute;
-    content: attr(data-hover);
-    color: #ffffff;
-    white-space: nowrap;
-    max-width: 0%;
-    overflow: hidden;
-  }
-
-  a:hover:before {
-    opacity: 1;
-    width: 100%;
-  }
-  a:hover:after {
-    max-width: 100%;
-  }
-
-  li {
-    font-family: var(--system-fonts);
-    margin: 0 5px;
-    padding: 10px;
-  }
-`
-
 const BurgerHolder = styled.div`
   @media screen and (min-width: 799px) {
     display: none;
@@ -90,16 +19,20 @@ const Menu = () => {
 
   return (
     <>
-      <DesktopHolder>
-        <DesktopMenu>
-          <li>
-            <Link to={`/about/`}>About</Link>
+      <nav className="header__nav">
+        <ul className="flex flex-row gap-5 py-5">
+          <li className="header__nav__item">
+            <Link to={`/about/`} className="block py-2 px-4">
+              About
+            </Link>
           </li>
-          <li>
-            <Link to={`/blog/`}>Blog</Link>
+          <li className="header__nav__item">
+            <Link to={`/blog/`} className="block py-2 px-4">
+              Blog
+            </Link>
           </li>
-        </DesktopMenu>
-      </DesktopHolder>
+        </ul>
+      </nav>
 
       <BurgerHolder>
         <BurgerMenu />
