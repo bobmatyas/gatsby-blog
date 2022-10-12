aa9b8a231628c5cc56c71727f2730e270c63555b
bob <45246438+bobmatyas@users.noreply.github.com>
2022-10-12 18:29:32 -0400

    adjust color contrast

--- src/components/home-post-link.tsx
+++ src/components/home-post-link.tsx
@@ -11,7 +11,7 @@ const BlogLink: React.FC<Props> = (props: Props) => (
   <article className="py-10 border-t-2 text-purple-500 border-stone-300 m-0 shadow-none h-entry no-underline hover:underline decoration-4 underline-offset-8 leading-loose">
     <h2>
       <Link to={props.link} className="u-uid u-url block">
-        <span className="dt-published font-sans text-lg font-bold">
+        <span className="dt-published font-sans text-lg font-extrabold">
           {props.date}:
         </span>
         <br />{" "}
